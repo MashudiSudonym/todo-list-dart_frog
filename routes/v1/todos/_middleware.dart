1e599f6f4e1726a616810afc9bc9670ce1bb7e05
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2023-08-11 20:51:50 +0700

    add cors header

--- routes/v1/todos/_middleware.dart
+++ routes/v1/todos/_middleware.dart
@@ -1,5 +1,6 @@
 import 'package:dart_frog/dart_frog.dart';
 import 'package:in_memory_todos_data_source/in_memory_todos_data_source.dart';
+import 'package:todos/middlewares/middlewares.dart';
 import 'package:todos_data_source/todos_data_source.dart';
 
 final _dataSource = InMemoryTodosDataSource();
@@ -7,5 +8,6 @@ final _dataSource = InMemoryTodosDataSource();
 Handler middleware(Handler handler) {
   return handler
       .use(requestLogger())
+      .use(corsHeaders())
       .use(provider<TodosDataSource>((_) => _dataSource));
 }
